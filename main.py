f7e36008760f6fdab5f667d526c547dd83896ec5
5568ke <5568keb67595@gmail.com>
2024-11-05 12:00:24 +0000

    feat : add register route for user

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+user_dict={}
+
+
+class User(BaseModel):
+    student_id : str
+    name : str
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/register")
+def register(user: User):
+    user_dict[user.student_id]=user.name
+    return {"message" : "register succussfully"}
